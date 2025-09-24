ff8ada937b847fe66b9ae3a67351dd7fcae85ab7
Max Lukovskyi <lostlonginus9sp@gmail.com>
2025-09-24 10:29:17 +0300

    fix

--- os-25/lab1-shell-scripting/variant6.sh
+++ os-25/lab1-shell-scripting/variant6.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 d=$1
-{ echo ""; find "$d" -mindepth 1 -type d -printf '%P\n'; } |
-while read -r r; do
-  n=${r:-$(basename "$d")}
-  p="$d/$r"
-  c=$(find "$p" -type f | wc -l)
-  s=$(find "$p" -type f -printf '%s\n' | awk '{t+=$1} END{print t+0}')
-  echo -e "$n\t$c\t$s"
-done | sort -t $'\t' -k2,2nr -k3,3nr -k1,1 | awk -F'\t' '{print $1" "$2" "$3}'
+
+
+files=$(find "$d" -type f | wc -l)
+size=$(du -sb "$d" | cut -f1)
+echo "$(basename "$d") $files $size"
+
+find "$d" -mindepth 1 -type d -printf '%P\n' | while read subdir; do
+    full_path="$d/$subdir"
+    files=$(find "$full_path" -type f | wc -l) 
+    size=$(du -sb "$full_path" | cut -f1)
+    echo "$subdir $files $size"
+done | sort -k2,2nr -k3,3nr
