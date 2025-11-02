01978c063fca677c20ae67de3d626411261570fd
Max Lukovskyi <lostlonginus9sp@gmail.com>
2025-11-02 15:23:23 +0200

    Замінено sed на awk через проблему з екрануванням символів

--- os-25/lab-2-regex/gplv2.c
+++ os-25/lab-2-regex/gplv2.c
@@ -122,15 +122,3 @@
 // signature of Moe Ghoul, 1 April 1989
 // Moe Ghoul, President of Vice
 // This General Public License does not permit incorporating your program into proprietary programs. If your program is a subroutine library, you may consider it more useful to permit linking proprietary applications with the library. If this is what you want to do, use the GNU Lesser General Public License instead of this License.
-
-
-
-#include <stdio.h>
-
-int main() {
-    printf("Hello World!\n");
-    return 0;
-}
-
-
-
--- os-25/lab-2-regex/update_license.sh
+++ os-25/lab-2-regex/update_license.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-grep -l "Version 1, February 1989" *.c | xargs sed -i 's/Version 1, February 1989/Version 2, June 1991/g'
+for f in *.c; do
+  [ "$f" = gplv1.c ] || [ "$f" = gplv2.c ] && continue
+  awk 'ARGIND==1{v1=v1$0 ORS;next} ARGIND==2{v2=v2$0 ORS;next} {s=s$0 ORS} END{p=index(s,v1); if(p) printf "%s%s%s", substr(s,1,p-1), v2, substr(s,p+length(v1)); else printf "%s", s}' \
+      gplv1.c gplv2.c "$f" > "$f.tmp" && mv "$f.tmp" "$f"
+done
